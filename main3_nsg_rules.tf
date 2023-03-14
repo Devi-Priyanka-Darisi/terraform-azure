ca5575e8c0bc6e677933d495c559b7d6e2d40348
Devi-Priyanka-Darisi <darisidevi.tech@gmail.com>
2023-03-14 07:47:26 +0530

    nsg

---
+++ main3_nsg_rules.tf
@@ -0,0 +1,10 @@
+# network security group
+resource "azurerm_network_security_group" "tf-nsg" {
+  name                = "tf-nsg"
+  location            = azurerm_resource_group.tf-rg.location
+  resource_group_name = azurerm_resource_group.tf-rg.name
+
+  tags = {
+    environment = "dev"
+  }
+}
